7b5c6e8900bc7e51e7a1eec8a139409ebc60dd29
Your Name <eytor-96@hotmail.com>
2021-09-02 10:31:34 +0000

    Algrím fyrir sequence verkefnið

---
+++ sequence.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+#Fer í for lykkju sem fer n sinnum yfir 
+#Gildið verður 1... 2...
+#Síðan er summan af 1 og 2 lögð saman
+#Eftir það er summan af 2 seinustu tölum lögð saman (3 og 2) en byrjar að bætast við +1
+#+1 hækkar síðan alltaf um 1 (+2... +3...) ásamt því að summa tvær seinustu tölurnar
+#Forritið skrifar síðan út rununa með kommu á milli frá 1-n.
