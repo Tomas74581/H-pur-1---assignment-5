1570a17def7d91d160e397bb290f03cc6dca40e7
AtliOskarsson <atlio@outlook.com>
2021-09-02 11:01:32 +0000

    spurning 2 forrit

--- sequence.py
+++ sequence.py
@@ -1,8 +1,21 @@
-n = int(input("Enter the length of the sequence: ")) # Do not change this line
-
 #Fer í for lykkju sem fer n sinnum yfir 
 #Gildið verður 1... 2...
 #Síðan er summan af 1 og 2 lögð saman
 #Eftir það er summan af 2 seinustu tölum lögð saman (3 og 2) en byrjar að bætast við +1
 #+1 hækkar síðan alltaf um 1 (+2... +3...) ásamt því að summa tvær seinustu tölurnar
 #Forritið skrifar síðan út rununa með kommu á milli frá 1-n.
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+first = 1
+second = 0
+third = 0
+sequence = 0
+for i in range(n):
+    sequence = first + second + third
+    third = second
+    second = first
+    first = sequence
+    if sequence == 2:
+        third = 0
+    print(sequence)
