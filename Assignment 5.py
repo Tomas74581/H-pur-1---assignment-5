8a0578255ed56b644b07f0aad22f1df2a4474c82
Tomas Eli Stefansson <tomaselistefans@gmail.com>
2021-09-02 10:13:22 +0000

    Instruction for algorithm

---
+++ Assignment 5.py
@@ -0,0 +1,5 @@
+# Algorithm
+# Make an algorithm that takes positive integers repeatively until a negative value is entered
+# When a negative value is entered, the maximum positive integer entered by the user should be printed 
+
+# Output should say The maximum is (max integer)
